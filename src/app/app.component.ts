d697c3a45c9e76ead535d9d0b502c63b3f0f7964
allan1989 <allan.rolle@laposte.net>
2025-02-11 14:46:42 +0100

    breadcrumbs component template complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FormComponent],
+  imports: [HeaderComponent, FormComponent, BreadcrumbsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/job-application-form/job-application-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-application-form',
+  imports: [],
+  templateUrl: './job-application-form.component.html',
+  styleUrl: './job-application-form.component.scss'
+})
+export class JobApplicationFormComponent {
+
+}
